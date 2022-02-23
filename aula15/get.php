23be95eb65e74677dfb3c0638ad338d81450555a
GuiDM <guilherme.extreme10@gmail.com>
2022-02-23 10:46:59 -0300

    .

---
+++ aula15/get.php
@@ -0,0 +1,25 @@
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aprendendo $_GET</title>
+    <style>
+        body {
+            text-align: center;
+        }
+    </style>
+
+</head>
+<body>
+
+    <form method="get" action="recebe_get.php">
+        <input type="text" name="nome" placeholder="Digite seu nome aqui" id=""><br><br>
+        <input type="text" name="idade" placeholder="Digite sua idade aqui" id=""><br><br>
+            <hr><button type="submit">enviar</button>
+    </form>
+    
+</body>
+</html>
---
+++ aula15/recebe_get.php
@@ -0,0 +1,7 @@
+<?php
+// METODO $_GET
+if (isset($_GET['nome']) && isset($_GET['idade'])){
+    echo "<h1>O nome é ". $_GET['nome']." e a idade ". $_GET['idade']."</h1>";
+}
+
+?>
